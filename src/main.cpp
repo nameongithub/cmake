ee03a1b99d0436694f84f76eeb8d3bd1483a193f
Paul <paulwongpang@foxmail.com>
2019-04-10 01:48:22 +0800

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@ using namespace std;
 #include"two.h"
 #include"sub.h"
 #include"multi.h"
+#include"div.h"
+#include"mod.h"
 int main(){
 	cout<<"hello"<<endl;
 	cout<<one()<<endl;
 	cout<<two()<<endl;
 	cout<<sub(34,2)<<endl;
 	cout<<multi(34,3)<<endl;
+	cout<<div(32,3)<<endl;
+	cout<<mod(64,32)<<endl;
 }
