4d2e3f334db990c97163fe74985dad79c3251331
Paul <paulwongpang@foxmail.com>
2019-04-10 01:25:35 +0800

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
 #include"one.h"
+#include"two.h"
+#include"sub.h"
+#include"multi.h"
 int main(){
 	cout<<"hello"<<endl;
 	cout<<one()<<endl;
+	cout<<two()<<endl;
+	cout<<sub(34,2)<<endl;
+	cout<<multi(34,3)<<endl;
 }
